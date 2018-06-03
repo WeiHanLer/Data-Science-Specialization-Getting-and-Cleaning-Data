71649cf0c67491e7f0f391f7aa77d7783b2cb584
wei han <whyler12@gmail.com>
2018-06-03 18:14:51 +0800

    run_analysis.R

---
+++ Run_Analysis.R
@@ -0,0 +1,3 @@
+X_train <- read.table("C:\Users\michael\Desktop\coursera\New folder\UCI HAR Dataset\train\X_train.txt")
+Y_train <- read.table("./UCI HAR Dataset/train/Y_train.txt")
+Sub_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
